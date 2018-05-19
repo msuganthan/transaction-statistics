04b35d1b5289d934d1934a29f485b8a6ac57e3b3
msuganthan <suganthanchandrika@gmail.com>
2018-05-20 01:20:11 +0530

    fix

--- src/main/java/com/suganthan/transactionstatistics/models/Transaction.java
+++ src/main/java/com/suganthan/transactionstatistics/models/Transaction.java
@@ -9,6 +9,9 @@ public class Transaction {
     private Double amount;
     private Long timestamp;
 
+
+    public Transaction() {}
+
     public Transaction(Double amount, Long timestamp) {
         this.amount = amount;
         this.timestamp = timestamp;
